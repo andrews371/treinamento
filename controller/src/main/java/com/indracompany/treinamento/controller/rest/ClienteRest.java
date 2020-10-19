7664dfcbe15fefffa933e2dc0ae08af1732b7e29
andrews371 <aoliveira371@gmail.com>
2020-10-18 22:04:56 -0300

    busca de clientes por ativo

--- controller/src/main/java/com/indracompany/treinamento/controller/rest/ClienteRest.java
+++ controller/src/main/java/com/indracompany/treinamento/controller/rest/ClienteRest.java
@@ -1,10 +1,13 @@
 package com.indracompany.treinamento.controller.rest;
 
+	
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,7 +30,20 @@ public class ClienteRest extends GenericCrudRest<Cliente, Long, ClienteService>{
 	@RequestMapping(value = "/buscar-por-cpf/{cpf}", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE})
 	public @ResponseBody ResponseEntity<Cliente> buscarClientePorCpf(final @PathVariable String cpf) {
 		Cliente retorno = clienteService.buscarClientePorCpf(cpf);
-		return  new ResponseEntity<>(retorno, HttpStatus.OK);
+		return new ResponseEntity<>(retorno, HttpStatus.OK);
+	}
+	
+	// a busca aqui é por uma lista
+	@RequestMapping(value = "/buscar-por-ativo/{ativo}", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE})
+	public @ResponseBody ResponseEntity<List<Cliente>> buscarClientePorAtivo(final @PathVariable boolean ativo) {
+		List<Cliente> retornoAtivo = clienteService.buscarClientePorAtivo(ativo);
+		return new ResponseEntity<List<Cliente>>(retornoAtivo, HttpStatus.OK);
+	}
+	
+	@Transactional
+	@RequestMapping(value = "/deletar-por-cpf/{cpf}", method = RequestMethod.DELETE, produces = { MediaType.APPLICATION_JSON_VALUE})
+	public @ResponseBody void deletarClientePorCpf(final @PathVariable String cpf) {
+		clienteService.deletarClientePorCpf(cpf);
 	}
 
 }
--- model/src/main/java/com/indracompany/treinamento/model/repository/ClienteRepository.java
+++ model/src/main/java/com/indracompany/treinamento/model/repository/ClienteRepository.java
@@ -1,9 +1,16 @@
 package com.indracompany.treinamento.model.repository;
 
+import java.util.List;
+
 import com.indracompany.treinamento.model.entity.Cliente;
 
 public interface ClienteRepository extends GenericCrudRepository<Cliente, Long> {
 
+	// MÉTODO GET EM UM ÚNICO ELEMENTO E EM UMA LISTA DE ELEMENTOS
 	Cliente findByCpf(String cpf);
+	List<Cliente> findByAtivo (boolean ativo);
+	
+	// MÉTODO DELETE 
+	Cliente deleteByCpf(String cpf);
 
 }
